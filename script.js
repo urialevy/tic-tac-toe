a129066747fb41b82840d17857edd5127cbb39e1
Uria <dorman010@gmail.com>
2023-08-02 16:57:03 +0100

    more work on grid click listener

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
+"use strict"
 // factory function for players
-function Player(name, symbol, active) {
+function Player(name, marker, active) {
     this.name = name;
-    this.symbol = symbol;
+    this.marker = marker;
     this.active = active;
     
   }
   
   const player1 = new Player("Player 1", 'X', true);
   const player2 = new Player("Player 2", 'O', false);
-  console.log(player1, player2);
   
-const toggleActive = (first, second) => {
-    const activeAnnounce = document.getElementById('active')
+
+
+const gameBoard = (() => {
+    const gridItems = document.querySelectorAll('.gridItem')
+    console.log(gridItems)
+    const addClick = () => {
+        gridItems.forEach(square => {
+            square.addEventListener('click', function(e){
+                displayController.toggleActive(player1, player2)
+            })
+        })
+    }
+    return {
+        addClick,
+    }
+})();
+
+const displayController = (() => {
+    const activeAnnounce = document.querySelector('#active')
+    const toggleActive = (first, second) => {
     first.active ? first.active = false : first.active = true
     second.active ? second.active = false : second.active = true
-    activeAnnounce.innerHTML = `<h2>Active player: ${first.active ? first.name : second.name}.</h2>`
-}  
-console.log(player1, player2);
+    activeAnnounce.innerHTML = `<h2>Active player: ${first.active ? first.name : second.name}.</h2>`}
+    toggleActive(player1, player2)
+    return {
+        activeAnnounce,
+        toggleActive
+    }
 
-// module function example
-/*
-const moduleCalc = (() => {
-  const add = (a,b) => a+b;
-  return {
-    a,b
-  }
-})
-*/
+})()
 
-const gameBoard = () => {};
-const displayController = () => {};
+gameBoard.addClick()
