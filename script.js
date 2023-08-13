ac8681f02f405b9012dfec130b45dd877c0639ee
Uria <dorman010@gmail.com>
2023-08-13 20:48:16 +0100

    adding dynamic style changes for game start/end

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ function Player(name, marker) {
     this.marker = marker;
 
   }
-  const player1 = new Player("Player 1", 'X');
-  const player2 = new Player("Player 2", 'O');
-
+  let player1 = new Player("Player 1", 'X');
+  let player2 = new Player("Player 2", 'O');
+  
 const gameBoard = (() => {
+    
     const gridItems = document.querySelectorAll('.gridItem')
+    const form = document.querySelector('#topForm');
+    const submitBtn = document.querySelector('#submitBtn')
+    const player1Name = document.querySelector('#player1Name');
+    const player2Name = document.querySelector('#player2Name');
+    
+    const toggleDisplay = () => {
+        let vis = false
+        vis == false ? document.querySelector('#grid').style.visibility = 'hidden' : document.querySelector('#grid').style.visibility = 'visible'
+        vis == true? vis === false : vis === true;
+        }
     const addClick = () => {
         gridItems.forEach(square => {
             square.addEventListener('click', function(e){
@@ -19,6 +30,16 @@ const gameBoard = (() => {
                 }
             })
         })
+    }
+    const setPlayerNames = () => {
+      submitBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        player1.name = player1Name.value == '' ? 'Player 1' : player1Name.value
+        player2.name = player2Name.value == '' ? 'Player 2' : player2Name.value
+        document.querySelector('#active').innerHTML = `<h2>Active player: ${currentPlayer.name}. Click anywhere in the UI to start.</h2>`
+        
+        toggleDisplay()
+      })
     }
     let currentPlayer = player1;
     let board = [];
@@ -40,7 +61,9 @@ const gameBoard = (() => {
         addMarker,
         gridItems,
         currentPlayer,
-        switchPlayer
+        switchPlayer,
+        setPlayerNames,
+        toggleDisplay
     
     }
 })();
@@ -53,15 +76,17 @@ const game = (() => {
     const winningPositions = [[0,1,2], [3,4,5], [6,7,8],[0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]]
     const checkWinningPosition = (player) =>{ 
         winningPositions.forEach(winningArray => winningArray.every(entry => gameBoard.gridItems[entry].innerHTML ==player.marker) ? winnerFound(player) : checkStalemate(remainingSpaces))}
-    const winnerFound = (player) => {document.querySelector('#winner').innerHTML = `<h1>Winner - ${player.name}! Reload the page to start a new game.</h1>`
-gameBoard.gridItems.forEach(item => item.innerHTML = '-')}
+    const winnerFound = (player) => {
+        document.querySelector('#active').innerHTML = ``;
+        document.querySelector('#winner').innerHTML = `<div><h1>Winner - ${player.name}! Reload the page to start a new game.</h1></div><div><button>New game</button></div>`
+        gameBoard.gridItems.forEach(item => item.innerHTML = '-')}
 const checkStalemate = (count) => {if (count < 1) document.querySelector('#winner').innerHTML = `<h1>Tie! Reload the page to try again.</h1>`}
     return {
         remainingSpaces,
         reduceSpaces,
         winningPositions,
         checkWinningPosition,
-        winnerFound
+        winnerFound,
     }
 
     
@@ -77,3 +102,5 @@ const displayController = (() => {
 })()
 
 gameBoard.addClick()
+gameBoard.setPlayerNames()
+gameBoard.toggleDisplay()
